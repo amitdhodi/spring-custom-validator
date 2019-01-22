04cd3e858354812625e02a6ba21c27758f377c36
Amit Dhodi <amit.dhodi1@tesco.com>
2019-01-22 14:16:19 +0530

    Added a numeric string custom validator

--- src/main/java/com/example/customvalidator/api/controller/ValidatorController.java
+++ src/main/java/com/example/customvalidator/api/controller/ValidatorController.java
@@ -8,13 +8,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
-
 @RestController
 public class ValidatorController {
 
     @PostMapping(value="/v1/validate", produces = "application/json")
-    public ResponseEntity<String> validateCustomerLocation(@Valid @RequestBody CustomerLocation customerLocation){
+    public ResponseEntity<String> validateCustomerLocation(@ValidCustomerLocation @RequestBody CustomerLocation customerLocation){
         return new ResponseEntity<>(HttpStatus.ACCEPTED);
     }
 }
